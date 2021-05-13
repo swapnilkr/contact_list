d92df09439d76bf9f9e568dfa8230dce0acf61ad
Swapnil <swapnilkr099@gmail.com>
2021-05-13 19:04:49 +0530

    Asset added

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+//req the library
+const mongoose=require('mongoose');
+
+// connect to database
+mongoose.connect('mongodb://localhost/contacts_list_db'); 
+
+// acquire the connection (to check if its successful)
+const db=mongoose.connection;
+
+// error
+db.on('error',console.error.bind(console,'error connecting to db'));
+
+// up and running then print the message
+db.once('open',function()
+{
+    console.log('Successfully connected to the database');
+});
+
---
+++ models/contact.js
@@ -0,0 +1,21 @@
+//req the library
+const mongoose=require('mongoose');
+
+// create schema
+const contactSchema=new mongoose.Schema({
+        name:{
+            type:String,
+            required:true
+        },
+        phone:{
+            type:String,
+            required:true
+        }
+    });
+
+const Contact = mongoose.model('Contact',contactSchema);
+
+module.exports = Contact;
+
+    
+
